924ca28ebe24260c7fc0fb22af73be6940393971
Bautax <bautax1234@gmail.com>
2022-07-14 10:25:02 -0300

    Program almost completed, pending implementation of undefined prompts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Returns a random choice from computer
+let possibleChoices = ["Scissors","Paper","Rock"];
 function computerPlay(){
-    return Math.floor(Math.random() * compOutcomes.length);
+    return Math.floor(Math.random() * possibleChoices.length);
 }
 //Makes selection case insensitive
 function caseInsensitive(input){
@@ -11,14 +12,25 @@ function caseInsensitive(input){
     else if(scissorsreg.test(input)) {return "Scissors";}
     else if(paperreg.test(input)) {return  "Paper";}
 }
-//One round of RPS
-let possibleChoices = ["Scissors","Paper","Rock"];
+//One round of Rock-paper-scissors
+
 let outcomes = ["You lost","Player wins","Draw","You lost","Player wins"]
 function playRound(uncheckedPlayerSelection,computerSelection){
     let playerSelection = possibleChoices.indexOf(caseInsensitive(uncheckedPlayerSelection));
     return outcomes[(playerSelection - computerSelection) + 2];
 }
-console.log(playRound("Rock",0));
 
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i ++){
+        let result = playRound(prompt("Rock paper or scissors: "), computerPlay());
+        console.log(result);
+        if(result == "You lost"){computerScore ++;}
+        else if(result == "Player wins"){playerScore ++;}
+    }
+    console.log("Player scores: ",playerScore,"Computer scores: ", computerScore);
+}
+game();
 
 

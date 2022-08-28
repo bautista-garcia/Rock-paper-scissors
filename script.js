bb67b052b0f49322f771000300e77ae713646990
Bautista <bautax1234@gmail.com>
2022-08-28 20:55:51 -0300

    Implemented three buttons that serve as selection

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//Returns a random choice from computer
-let possibleChoices = ["Scissors","Paper","Rock"];
-function computerPlay(){
-    return Math.floor(Math.random() * possibleChoices.length);
-}
-//Makes selection case insensitive
-function caseInsensitive(input){
+
+/*function caseInsensitive(input){
     let rockreg = /Rock/i;
     let scissorsreg = /Scissors/i;
     let paperreg = /paper/i;
@@ -12,25 +7,41 @@ function caseInsensitive(input){
     else if(scissorsreg.test(input)) {return "Scissors";}
     else if(paperreg.test(input)) {return  "Paper";}
 }
-//One round of Rock-paper-scissors
+*/
 
+const possibleChoices = ["Rock","Paper","Scissors"];
+function computerPlay(){
+    return Math.floor(Math.random() * possibleChoices.length);
+}
 let outcomes = ["You lost","Player wins","Draw","You lost","Player wins"]
-function playRound(uncheckedPlayerSelection,computerSelection){
-    let playerSelection = possibleChoices.indexOf(caseInsensitive(uncheckedPlayerSelection));
-    return outcomes[(playerSelection - computerSelection) + 2];
+//Adjust playRound algorithm to display the correct result
+function playRound(playerSelection,computerSelection){
+    console.log(outcomes[(playerSelection - computerSelection) + 2]);
 }
 
-function game(){
+const rockChoice = document.getElementById('rock');
+const paperChoice = document.getElementById('paper');
+const scissorsChoice = document.getElementById('scissors');
+
+rockChoice.addEventListener('click',playRound.bind(this,0,computerPlay()));
+paperChoice.addEventListener('click',playRound.bind(this,1,computerPlay()));
+scissorsChoice.addEventListener('click',playRound.bind(this,2,computerPlay()));
+
+
+
+
+
+/*function game(){
     let playerScore = 0;
     let computerScore = 0;
     for(let i = 0; i < 5; i ++){
         let result = playRound(prompt("Rock paper or scissors: "), computerPlay());
-        console.log(result);
+        result);
         if(result == "You lost"){computerScore ++;}
         else if(result == "Player wins"){playerScore ++;}
     }
-    console.log("Player scores: ",playerScore,"Computer scores: ", computerScore);
+    "Player scores: ",playerScore,"Computer scores: ", computerScore);
 }
 game();
-
+*/
 
